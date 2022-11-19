cacf20cf561599d33e6c07103411d03dd0f07240
socrates07 <screspo@merkatu.com>
2022-11-19 19:23:18 +0100

    Cambiando nombre de la aplicación

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# NasaView
+# MyNasa
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.1.2.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'nasaView'`, () => {
+  it(`should have as title 'MyNasa'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('nasaView');
+    expect(app.title).toEqual('MyNasa');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('nasaView app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('MyNasa app is running!');
   });
 });
